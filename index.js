caa213fb93db37f704cc2a9d239ccc000d3f5c0c
rafi-zimraan <raffizimraan27@gmail.com>
2023-02-18 11:19:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+// A.
+class car  {
+    nameCar = " Toyota Supra"
+    typeCar = "racing"
+    yearCar = 1990
+
+        hasilCar() {
+        console.log(`nama mobil ${this.nameCar} dengan type ${this.typeCar} dan tahun di buat ${this.yearCar}`)
+    }
+}
+
+const eliminasi = new car();
+console.log(eliminasi);
+
+// B.
+class food  {
+    nameFood = "pizza"
+    typeFood = "meksico"
+    yearCar = 2023
+
+        hasilFood() {
+        console.log(`nama makanan ${this.nameFood} dengan type ${this.typeFood} dan tahun di buat ${this.typeFood}`)
+    }
+}
+
+const eliminasi2 = new food();
+console.log(eliminasi2);
+
+// C.
+class hotel  {
+    nameHotel = "the royal zimraan"
+    typeHotel = "kelas atas"
+    yearHotel = 2222
+
+        hasilHotel() {
+        console.log(`nama hotel ${this.nameHotel} dengan type ${this.typeHotel} dan tahun di buat ${this.yearHotel}`)
+    }
+}
+
+const eliminasi3 = new hotel();
+console.log(eliminasi3);
+
+
+// D.
+class jalan  {
+    nameJalan = "kH rafi zimraan lc."
+    typeJalan= "racing"
+    yearJalan = 1232
+    
+    hasilJalan() {
+        console.log(`nama jalan ${this.nameJalan} dengan type ${this.typeJalan} dan tahun di buat ${this.yearJalan}`)
+    }
+}
+const eliminasi4 = new jalan();
+console.log(eliminasi4);
+
+
+
+// E.
+class pondok {
+    namePondok = " pondok taper"
+    typePondok = "Ldti"
+    yearPondok = 1990
+
+        hasilPondok() {
+        console.log(`nama pondok ${this.namePondok} dengan type ${this.typePondok} dan tahun di buat ${this.yearPondok}`)
+    }
+}
+const eliminasi5 = new pondok();
+console.log(eliminasi5);
+
+
+
